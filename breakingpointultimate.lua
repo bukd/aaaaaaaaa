ddcffc952611b7eb513ea4855edb54a7d918bbf4
bukd <71043544+bukd@users.noreply.github.com>
2022-09-30 15:29:59 -0300

    breakingpointultimate.lua

---
+++ breakingpointultimate.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ColdStep2/Breaking-Point-Funny-Squid-Hax/main/Breaking%20Point%20Funny%20Squid%20Hax",true))();
+loadstring(game:HttpGet("https://pastebin.com/raw/fFmJFepC"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/bukd/aaaaaaaaa/master/breaking%20point%20all%20hit.lua"))()
